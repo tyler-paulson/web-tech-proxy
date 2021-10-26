56887457ec4a35fb15f17a54724ccb36f65bf925
Tyler Paulson <me@tylerpaulson.com>
2021-10-26 12:19:24 -0400

    Change header

--- netlify/functions/get-random-coffee/get-random-coffee.js
+++ netlify/functions/get-random-coffee/get-random-coffee.js
@@ -5,7 +5,7 @@ exports.handler = async (event, context) => {
   return {
     statusCode: 200,
     headers: {
-      'Content-Type': 'text/plain',
+      'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Headers': 'Content-Type',
       'Access-Control-Allow-Methods': 'GET'
