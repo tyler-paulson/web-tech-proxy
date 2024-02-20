dfab85c9cf7b222d015300c2ee495d76f038b334
Tyler Paulson <me@tylerpaulson.com>
2024-02-20 12:38:37 -0500

    Add year parameter to hockey stats

--- netlify/functions/get-goals-for/get-goals-for.js
+++ netlify/functions/get-goals-for/get-goals-for.js
@@ -3,12 +3,13 @@ const https = require('https')
 
 exports.handler = async (event, context) => {
   const team = event.queryStringParameters.team.replace(/\s/g, '').toLowerCase()
+  const year = event.queryStringParameters.year.replace(/\s/g, '').toLowerCase()
   const opts = {
     httpsAgent: new https.Agent({
       rejectUnauthorized: false
     })
   }
-  const res = await axios.get('https://morehockeystats.com/teams/en?table=1&season=2021&stage=2&view=against&page=1&hl=', opts)
+  const res = await axios.get('https://morehockeystats.com/teams/en?table=1&season='+year+'&stage=2&view=against&page=1&hl=', opts)
   var ret = {};
   res.data.data.forEach((item, index) => {
     console.log(`${index} : ${item}`);
